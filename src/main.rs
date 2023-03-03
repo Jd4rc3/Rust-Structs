71c6ac1218a5c26cbad116bf1c1cc8c5f723c1a4
Juan Daniel <juan.arce@sofka.com.co>
2023-03-03 17:28:04 -0500

    Associated functions with differents impl block

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,22 @@ impl Rectangle {
     }
 }
 
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
     let rect2 = Rectangle { width: 10, height: 40 };
     let rect3 = Rectangle { width: 60, height: 45 };
+    let square1 = Rectangle::square(10);
     println!("Can rect1 hold react2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold react3? {}", rect1.can_hold(&rect3));
+
+    println!("{:#?}", square1);
 }
